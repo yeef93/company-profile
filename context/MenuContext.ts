d4dbc7f7a46f859bfb03bfa0530d8a911d92f92d
yeef93 <yunindafaranika@gmail.com>
2024-05-20 16:41:17 +0700

    feat:addmenucontext

--- context/MenuContext.ts
+++ context/MenuContext.ts
@@ -1,3 +1,16 @@
-export default function MenuContext(){
-    return{}
+"use client"
+import { createContext } from "react";
+
+const defaultValue = {
+  menuShowing: false,
+  setShowing: () => {},
 }
+
+interface MenuContextType {
+  menuShowing: boolean;
+  setShowing: () => void;
+}
+
+const MenuContext = createContext<MenuContextType>(defaultValue);
+
+export default MenuContext;
--- utils/program.ts
+++ utils/program.ts
@@ -4,6 +4,7 @@ export default [
     id: 0,
     image: "cuisine.webp",
     programName: "Cuisine Program",
+    groupName: "Cuisine",
     detail:
       "Master the art of cooking with our six-month Cuisine Program, covering essential techniques and international cuisines.",
   },
@@ -11,13 +12,15 @@ export default [
     id: 1,
     image: "patisserie.webp",
     programName: "Patisserie Program",
+    groupName: "Patisserie",
     detail:
       "Perfect your baking and pastry skills in our six-month Patisserie Program, from basic baking to advanced pastry techniques.",
   },
   {
     id: 2,
     image: "barista.webp",
-    groupName: "Barista Program",
+    programName: "Barista Program",
+    groupName: "Barista",
     detail:
       "Excel in the coffee industry with our six-month Barista Program, focusing on brewing techniques, latte art, and coffee shop management.",
   },
