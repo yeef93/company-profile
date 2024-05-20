90b79599077f9817007013df44fd22751055de38
yeef93 <yunindafaranika@gmail.com>
2024-05-20 15:26:24 +0700

    feat:updateprogram

--- app/components/Programs/index.tsx
+++ app/components/Programs/index.tsx
@@ -1,35 +1,38 @@
 import TwoContent from "@/components/TwoContent";
 import Image from "next/image";
 import Button from "@/components/Button";
-import cuisine from "@/public/assets/program/cuisine.webp"
+import programdata from "@/utils/program";
 
 function Program() {
   return (
     <section className=" h-full">
       <TwoContent title="Bon Appétit Programs">
-        <div className=" grid grid-cols-1 gap-4">
-          <a
-            href="#"
-            className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 "
-          >
-            <Image
-              className=" w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-              src={cuisine}
-              alt=""
-              width={300}
-              height={300}
-            />
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                Noteworthy technology acquisitions 2021
-              </h5>
-              <p className="mb-3 font-normal text-gray-700 ">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
-            </div>
-            <Button text="See Programs" />
-          </a>
+        <div className=" grid grid-cols-1 gap-10">
+          {programdata.map((item) => {
+            return (
+              <a key={item.id}
+                href="#"
+                className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 "
+              >
+                <Image
+                  className=" w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                  src={`/assets/program/${item.image}`}
+                  alt=""
+                  width={300}
+                  height={300}
+                />
+                <div className="flex flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                    {item.programName}
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 ">
+                    {item.detail}
+                  </p>
+                </div>
+                <Button text="See Programs" />
+              </a>
+            );
+          })}
         </div>
       </TwoContent>
     </section>
--- utils/program.ts
+++ utils/program.ts
@@ -1,24 +1,24 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default[
-    {
-        id: 0,
-        image: "cuisine.webp",
-        groupName: "Cuisine",
-        programName: "Cuisine Class",   
-        detail:""
-    },
-    {
-        id: 1,
-        image: "patisserie.webp",
-        groupName: "Patisserie",
-        programName: "Patisserie Class",   
-        detail:""  
-    },
-    {
-        id: 2,
-        image: "barista.webp",
-        groupName: "Barista",
-        programName: "Celebrity Chef",   
-        detail:""
-    }
-]
+export default [
+  {
+    id: 0,
+    image: "cuisine.webp",
+    programName: "Cuisine Program",
+    detail:
+      "Master the art of cooking with our six-month Cuisine Program, covering essential techniques and international cuisines.",
+  },
+  {
+    id: 1,
+    image: "patisserie.webp",
+    programName: "Patisserie Program",
+    detail:
+      "Perfect your baking and pastry skills in our six-month Patisserie Program, from basic baking to advanced pastry techniques.",
+  },
+  {
+    id: 2,
+    image: "barista.webp",
+    groupName: "Barista Program",
+    detail:
+      "Excel in the coffee industry with our six-month Barista Program, focusing on brewing techniques, latte art, and coffee shop management.",
+  },
+];
