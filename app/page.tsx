2492dba25c959a917c7d3d0df63b487425de8783
yeef93 <yunindafaranika@gmail.com>
2024-05-20 15:08:55 +0700

    feat:programs

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Testimonials from "./components/Testimonials";
 import Faq from "./components/Faq";
 import About from "./components/About";
 import Promote from "./components/Promote";
-import Programs from "./programs/page";
+import Programs from "./components/Programs";
 import TeamList from "./components/TeamList";
 
 export default function Home() {
--- utils/program.ts
+++ utils/program.ts
@@ -2,31 +2,23 @@
 export default[
     {
         id: 0,
-        image: "Decodable.co",
+        image: "cuisine.webp",
         groupName: "Cuisine",
         programName: "Cuisine Class",   
         detail:""
     },
     {
         id: 1,
-        image: "Decodable.co",
+        image: "patisserie.webp",
         groupName: "Patisserie",
         programName: "Patisserie Class",   
         detail:""  
     },
     {
         id: 2,
-        image: "Decodable.co",
+        image: "barista.webp",
         groupName: "Barista",
         programName: "Celebrity Chef",   
         detail:""
-    },
-    {
-        id: 3,
-        image: "Decodable.co",
-        groupName: "Barista",
-        programName: "Barista Class With Viki Rahardja",   
-        detail:""  
-         
     }
 ]
